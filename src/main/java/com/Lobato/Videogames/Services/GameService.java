356d49a40af1d8b900444c8b7ded8c30888b3de2
Luis Lobato <luislobato.medinaeduardo@gmail.com>
2026-02-25 15:19:32 -0600

    Este commit no deberia hacerlo en la rama main xd, de todas maneras iba a hacer un merge pero xd, modificacion del gameService, le añadi un metodo, aun no esta terminado

--- src/main/java/com/Lobato/Videogames/Services/GameService.java
+++ src/main/java/com/Lobato/Videogames/Services/GameService.java
@@ -1,4 +1,18 @@
 package com.Lobato.Videogames.Services;
 
+
+import com.Lobato.Videogames.Infraestructure.GameInfraestructure;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GameService {
+
+    @Autowired
+    private GameInfraestructure gameInfraestructure;
+
+    public void addNewVideogame(){
+
+    }
+
 }
